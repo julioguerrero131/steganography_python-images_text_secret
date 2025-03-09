7671f8134b6f516ab29b952a8ccbcde076455fef
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2025-03-09 18:39:56 -0500

    add: configuraciones root

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import os
 from stegano import lsb
+
+root = Tk()
+root.title("Esteganografía - Oculta Mensajes de Texto en una Imagen")
+root.geometry("700x500+150+180")
+root.resizable(False, False)
+root.configure(bg="#2f4155")
+
+# icon
+image_icon = PhotoImage(file="logo.jpg") 
+root.iconphoto(False, image_icon)
+
+# logo
+logo = PhotoImage(file="logo.png")
+Label(root, image=logo, bg="#2f4155").place(x=10, y=0)
+
+root.mainloop()
